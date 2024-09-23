aaa7bc4078c733d9f05099f105a3e414f2400e65
SimbaWANGU <simbawangulu@gmail.com>
2024-09-23 13:32:23 +0300

    update icons

---
+++ src/components/icons/TabIcons.tsx
@@ -0,0 +1,23 @@
+import { Platform } from 'react-native'
+import React from 'react'
+import { FontAwesome6 } from '@expo/vector-icons'
+import { View } from '@components/styled/Themed'
+
+interface TabsIconsProps {
+	name: React.ComponentProps<typeof FontAwesome6>['name']
+	color: string
+	focused?: boolean
+}
+
+const TabsIcons: React.FC<TabsIconsProps> = ({ name, color, focused }) => {
+
+	return (
+		<View
+			className={`h-full w-full ${Platform.OS === 'ios' ? focused ? 'mt-8' : 'mt-8' : 'mt-2'} flex flex-col bg-transparent text-center items-center justify-evenly`}
+		>
+			<FontAwesome6 size={20} name={name} color={color} />
+		</View>
+	)
+}
+
+export default TabsIcons
