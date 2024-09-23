4a6c9896280cddcd41bf1c9b7c7659b8e6ef3a40
SimbaWANGU <simbawangulu@gmail.com>
2024-09-23 13:26:42 +0300

    update app utils

---
+++ src/utils/Utils.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.png' {
+  const value: any
+  export = value;
+}
+
+declare module '*.otf' {
+  const value: any
+  export = value
+}
+
+declare module '*.ttf' {
+  const value: any
+  export = value
+}
