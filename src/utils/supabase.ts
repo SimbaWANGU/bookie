e186bb1e431a2c5acf28adffcf47a5adf61b59ae
SimbaWANGU <simbawangulu@gmail.com>
2025-02-25 20:51:07 +0300

    add supabase configurations

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -5,6 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string
 
+console.log(supabaseUrl, supabaseAnonKey)
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 	auth: {
 		storage: AsyncStorage,
