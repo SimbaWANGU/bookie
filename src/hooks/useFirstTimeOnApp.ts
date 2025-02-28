fdefcaa0bb5d44316ad3347aeff87f4a2e0269bc
SimbaWANGU <simbawangulu@gmail.com>
2025-02-28 11:41:30 +0300

    remove unused files

--- src/hooks/useFirstTimeOnApp.ts
+++
@@ -1,9 +0,0 @@
-import { useAppStore } from '@stores/useAppStore'
-
-const useFirstTimeOnApp = (): [boolean, (firstTimeOnApp: boolean) => void] => {
-  const [firstTimeOnApp, setFirstTimeOnApp] = useAppStore((state) => [state.firstTimeOnApp, state.setFirstTimeOnApp])
-
-  return [firstTimeOnApp, setFirstTimeOnApp]
-}
-
-export default useFirstTimeOnApp
