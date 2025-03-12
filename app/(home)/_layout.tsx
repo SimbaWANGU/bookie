76dad3251df89cab653d1dd5bb442ecbfb3bc5af
SimbaWANGU <simbawangulu@gmail.com>
2025-03-12 11:06:28 +0300

    minor update

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,104 +1,105 @@
-import React, { useEffect } from 'react'
-import { Redirect, Tabs } from 'expo-router'
-import { useColorScheme } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Platform, useColorScheme } from 'react-native'
 import { light, dark } from '@constants/Color'
-import { useQuery } from '@tanstack/react-query'
-import { supabase } from '@utils/supabase'
 import Header from '@components/headers/header'
 import ProfileHeader from '@components/headers/profileHeader'
-import { userProfile } from '@models/userProfile.type'
 import { getDynamicValue } from '@constants/Functions'
 import TabsIcons from '@components/icons/TabIcons'
-import useFirstTimeOnApp from '@hooks/useFirstTimeOnApp'
+import { useAtom } from 'jotai'
+import { userAtom } from '@stores/user.state'
+import { firstTimeOnAppAtom } from '@stores/firstTimeonApp.state'
+import Onboard from 'app/auth/onboard'
+import AuthScreen from 'app/auth/authenticate'
+import ContinueReading from '@components/PageComponents/home/ContinueReading'
 
 export default function TabLayout() {
 	const theme = useColorScheme()
-	const [firstTimeOnApp] = useFirstTimeOnApp()
+	const [firstTimeOnApp] = useAtom(firstTimeOnAppAtom)
+	const [user] = useAtom(userAtom)
 
-	return <Redirect href="/auth/onboard" />
-	// if (firstTimeOnApp) {
-	// }
+	// console.log(firstTimeOnApp, user)
 
-	// const { isLoading, data, error } = useQuery({
-	// 	queryKey: [`user-${session?.user.id}`],
-	// 	queryFn: async () => {
-	// 		const { data, error } = await supabase.from('profiles').select('*').eq('id', session?.user.id).single()
-	// 		if (error) {
-	// 			// Sentry.Native.captureMessage('Error catched from get user profile')
-	// 			// Sentry.Native.captureException(error)
-	// 			return {} as userProfile
-	// 		}
-	// 		return data as userProfile
-	// 	}
-	// })
-
-	// useEffect(() => {
-	// 	if (data) setUser(data)
-	// 	const fetchProfilePicture = async () => {  
-	// 		const blob = await supabase.storage.from('avatars').download(`${session?.user?.id}/pp`)
-	// 		const fr = new FileReader()
-	// 		if (blob.data){
-	// 			fr.readAsDataURL(blob.data!)
-	// 			fr.onload = () => {
-	// 				setProfilePicture(fr.result as string)
-	// 			}	
-	// 		}
-	// 	}
-
-	// 	fetchProfilePicture()
-	// }, [isLoading, error])
 
 	return (
-		<Tabs
-			screenOptions={{
-				tabBarStyle: {
-					height: getDynamicValue(100),
-					justifyContent: 'center',
-					alignItems: 'center',
-					backgroundColor: theme === 'dark' ? dark.background : light.background,
-				},
-				tabBarInactiveTintColor: theme === 'dark' ? dark.iconsColor : light.iconsColor,
-				tabBarActiveTintColor: theme === 'dark' ? dark.activeIconColor : light.activeIconColor,
-			}}>
-			<Tabs.Screen
-				name="index"
-				options={{
-					headerShown: true,
-					header: () => (
-						<Header
-							username={'username'}
-							avatarUrl={'https://ui-avatars.com/api/?name=U+N'}
-						/>
-					),
-					tabBarIcon: ({ color }) => <TabsIcons name="house" color={color} />,
-					title: ''
-				}}
-			/>
-
-			<Tabs.Screen
-				name="search"
-				options={{
-					headerShown: true,
-					header: () => (
-						<Header
-							username={'username'}
-							avatarUrl={'https://ui-avatars.com/api/?name=U+N'}
-						/>
-					),
-					tabBarIcon: ({ color }) => <TabsIcons name="magnifying-glass" color={color} />,
-					title: ''
-				}}
-			/>
-			
-			<Tabs.Screen
-				name="profile"
-				options={{
-					headerShown: true,
-					header: () => <ProfileHeader />,
-					tabBarIcon: ({ color }) => <TabsIcons name="user" color={color} />,
-					title: ''
-				}}
-			/>
-		</Tabs>
+		<>
+			{(() => {
+        if (firstTimeOnApp && !user) {
+          return (
+						<>
+							<AuthScreen />
+						</>
+					)
+        } else if (!firstTimeOnApp && !user) {
+          return (
+						<>
+							<Onboard />
+						</>
+					)
+        } else {
+          return (
+						(
+							<>
+								<Tabs
+									screenOptions={{
+										tabBarStyle: {
+											height: Platform.OS === 'ios' ? getDynamicValue(120) : getDynamicValue(100),
+											justifyContent: 'center',
+											alignItems: 'center',
+											backgroundColor: theme === 'dark' ? dark.background : light.background,
+										},
+										tabBarInactiveTintColor: theme === 'dark' ? dark.iconsColor : light.iconsColor,
+										tabBarActiveTintColor: theme === 'dark' ? dark.activeIconColor : light.activeIconColor,
+									}}>
+									<Tabs.Screen
+										name="index"
+										options={{
+											headerShown: true,
+											header: () => (
+												<Header
+													username={user?.user_metadata.full_name}
+													avatarUrl={'https://ui-avatars.com/api/?name=U+N'}
+												/>
+											),
+											tabBarIcon: ({ color }) => <TabsIcons name="house" color={color} />,
+											title: '',
+											lazy: false
+										}}
+									/>
+					
+									<Tabs.Screen
+										name="search"
+										options={{
+											headerShown: true,
+											header: () => (
+												<Header
+													username={user?.user_metadata.full_name}
+													avatarUrl={'https://ui-avatars.com/api/?name=U+N'}
+												/>
+											),
+											tabBarIcon: ({ color }) => <TabsIcons name="magnifying-glass" color={color} />,
+											title: '',
+											lazy: false,
+										}}
+									/>
+									
+									<Tabs.Screen
+										name="profile"
+										options={{
+											headerShown: true,
+											header: () => <ProfileHeader />,
+											tabBarIcon: ({ color }) => <TabsIcons name="user" color={color} />,
+											title: '',
+											lazy: false
+										}}
+									/>
+								</Tabs>
+								<ContinueReading />
+							</>
+						)
+					)
+        }
+      })()}
+		</>
 	)
 }
