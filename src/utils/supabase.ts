59e88ef7db29b9c2c45a8f1d90c4e13c28a4076b
SimbaWANGU <simbawangulu@gmail.com>
2025-04-16 15:21:39 +0300

    rewrite storage implementation with secure store

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,15 +1,14 @@
 import 'react-native-url-polyfill/auto'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { AppState } from 'react-native'
+import { secureStoreAdapter } from './secureStoreAdapter'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 	auth: {
-		//! convert to secure store
-		storage: AsyncStorage,
+		storage: secureStoreAdapter,
 		autoRefreshToken: true,
 		persistSession: true,
 		detectSessionInUrl: false,
