4f1c02a0c0ed8567127031b802ef9bd413024b83
SimbaWANGU <simbawangulu@gmail.com>
2025-05-20 12:24:38 +0300

    add notification registration to Main.tsx file

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Slot, SplashScreen } from 'expo-router'
 import { CustomUser } from '@models/userProfile.type'
 import { userAtom } from '@stores/user.state'
 import { useAtom } from 'jotai'
-import { useQuery } from '@tanstack/react-query'
+import { QueryClient, useQuery, useQueryClient } from '@tanstack/react-query'
 import { fetchCustomUser } from '@api/profile/api.user'
 import { QueryKeys } from '@constants/QueryKeys'
 import { firstTimeOnAppAtom } from '@stores/firstTimeonApp.state'
+import * as Notifications from 'expo-notifications'
+import { registerForPushNotificationsAsync } from '@hooks/usePushNotifications'
 
 const Main = () => {
+  const queryClient = useQueryClient()
   const [, setSession] = useAtom(userAtom)
   const [firstTimeOnApp] = useAtom(firstTimeOnAppAtom)
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState<Notifications.Notification | undefined>(undefined)
 
   const { data, isLoading } = useQuery<CustomUser>({
     queryKey: [QueryKeys.getUser],
@@ -32,6 +37,24 @@ const Main = () => {
       SplashScreen.hideAsync()
     }
   }, [isLoading])
+
+	
+  // notification setup
+	useEffect(() => {
+		registerForPushNotificationsAsync().then(token => setExpoPushToken(token ?? '')).catch((error) => setExpoPushToken(`${error}`));
+		const notificationListener = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+		return () => {
+			notificationListener.remove()
+			responseListener.remove()
+		}
+	}, [])
 
 
   if (isLoading) {
