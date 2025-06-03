0bb524551da639ffb71461035339baa15c567094
SimbaWANGU <simbawangulu@gmail.com>
2025-06-03 09:10:48 +0300

    add club search term atom

--- src/stores/search.state.ts
+++ src/stores/search.state.ts
@@ -3,10 +3,12 @@ import { atom } from 'jotai'
 type SearchOptionProps = 'users' | 'authors' | 'books' | 'pages' | ''
 
 const searchTermAtom = atom('')
+const clubSearchTermAtom = atom('')
 const searchOptionsAtom = atom<SearchOptionProps>('')
 
 export {
   searchTermAtom,
+  clubSearchTermAtom,
   searchOptionsAtom,
   type SearchOptionProps
 }
