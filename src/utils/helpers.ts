144047f419a1ab173e45bf679383a2d43ff820af
SimbaWANGU <simbawangulu@gmail.com>
2025-06-03 09:04:49 +0300

    add text size helper functions to manage text size across different screen sizes

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,2 +1,22 @@
 // TODO: Add helpers
 export {}
+
+// screenUtils.ts
+import { Dimensions } from 'react-native'
+
+type ScreenSizeCategory = 'small' | 'medium' | 'large'
+
+const getScreenSizeCategory = (): ScreenSizeCategory => {
+  const { height, width } = Dimensions.get('window')
+  const screenArea = height * width
+// 844 390
+  console.log(height, width)
+
+  if (screenArea < 850 * 390) return 'small'
+  if (screenArea < 1100 * 500) return 'medium'
+  return 'large'
+}
+
+export {
+  getScreenSizeCategory
+}
