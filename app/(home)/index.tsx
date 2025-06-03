925d07d0a9cc6b8047c10070b7a71dcbd09915fd
SimbaWANGU <simbawangulu@gmail.com>
2025-06-03 09:14:13 +0300

    convert home page from flatlist to sectionlist

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,37 +1,53 @@
 import React from 'react'
-import { View, FlatList, RefreshControl } from 'react-native'
-import { useColorScheme } from 'react-native'
+import { View, SectionList, Text, useColorScheme } from 'react-native'
 import tw from '@utils/tailwind'
 import FeaturedBooks from '@components/PageComponents/home/FeaturedBooks'
 import UserActivities from '@components/PageComponents/home/UserActivities'
 import ActivityFilter from '@components/PageComponents/home/ActivityFilter'
-import Header from '@components/headers/header'
 
 const sections = [
-  // { key: 'friends', component: <OthersStartedReadingStories />},
-  { key: 'header', component: <Header /> },
-  { key: 'featured', component: <FeaturedBooks /> },
-  // { key: 'filter', component: <ActivityFilter /> },
-  { key: 'endless_scroll', component: <UserActivities /> },
+  {
+    title: 'Featured Books',
+    data: ['featured'],
+    render: () => <FeaturedBooks />,
+  },
+  // Uncomment this if needed
+  // {
+  //   title: 'Filter',
+  //   data: ['filter'],
+  //   render: () => <ActivityFilter />,
+  // },
+  {
+    title: 'My Feed',
+    data: ['endless_scroll'],
+    render: () => <UserActivities />,
+  },
 ]
 
 const Index = () => {
   const theme = useColorScheme()
-  
-  const renderItem = ({ item }) => (
-    <View style={tw`mb-4`}>
-      {item.component}
-    </View>
-  )
 
   return (
     <View style={tw`flex-1 ${theme === 'light' ? 'bg-light' : 'bg-dark'}`}>
-      <FlatList
-        style={tw``}
-        data={sections}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.key}
-        contentContainerStyle={{ padding: 16 }}
+      <SectionList
+        sections={sections}
+        stickySectionHeadersEnabled={false}
+        keyExtractor={(item, index) => item + index}
+        contentContainerStyle={tw`p-4 android:mt-32 ios:mt-24`}
+        renderItem={({ section }) => (
+          <View style={tw`mb-4`}>
+            {section.render()}
+          </View>
+        )}
+        renderSectionHeader={({ section: { title } }) => {
+          if (title === 'Featured Books') {
+            return <></>
+          }
+          return (
+            <Text style={tw`text-lg font-bold android:my-4 ios:my-1 ${theme === 'light' ? 'text-dark' : 'text-light'}`}>
+              {title}
+            </Text>
+        )}}
       />
     </View>
   )
