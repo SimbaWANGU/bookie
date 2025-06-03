920d23ef7cd34a1d71b38ba3992d85cc559ad240
SimbaWANGU <simbawangulu@gmail.com>
2025-06-03 09:09:08 +0300

    update progress query function

--- src/api/story/api.progress.ts
+++ src/api/story/api.progress.ts
@@ -1,15 +1,15 @@
 import { supabase } from '@utils/supabase'
 
 interface ReadingProgress {
-  book_id: string
-  paragraph_id: string
-  paragraph_no: number
-  user_id: string
-  started_at: Date
-  last_updated_at: Date
-  completed_at: Date
-  total_time_spent: number
-  status: string
+  book_id?: string
+  paragraph_id?: string
+  paragraph_no?: number
+  user_id?: string
+  started_at?: Date
+  last_updated_at?: Date
+  completed_at?: Date
+  total_time_spent?: number
+  status?: string
 }
 
 const checkReadingProgress = async (book_id: string, user_id: string) => {
@@ -23,7 +23,7 @@ const checkReadingProgress = async (book_id: string, user_id: string) => {
   return data
 }
 
-const initialReadingProgress = async ({ book_id, paragraph_id, paragraph_no = 1, user_id, started_at}: ReadingProgress) => {
+const initialReadingProgress = async ({ book_id, paragraph_id, paragraph_no = 1, user_id, started_at, total_time_spent }: ReadingProgress) => {
   const { data, error } = await supabase.from('user_reading_progress').insert([
     {
       book_id,
@@ -31,6 +31,7 @@ const initialReadingProgress = async ({ book_id, paragraph_id, paragraph_no = 1,
       current_paragraph: paragraph_no,
       user_id,
       started_at,
+      total_time_spent,
       last_updated_at: started_at,
       status: 'STARTED'
     },
@@ -42,26 +43,43 @@ const initialReadingProgress = async ({ book_id, paragraph_id, paragraph_no = 1,
   return data
 }
 
-const updateReadingProgress = async (progress: ReadingProgress) => {
-  const { book_id, paragraph_id, user_id, paragraph_no, last_updated_at, status } = progress
+const updateReadingProgress = async (progress: ReadingProgress, state: 'UPDATED' | 'COMPLETED') => {
+  const { book_id, paragraph_id, user_id, paragraph_no, last_updated_at, status, completed_at, total_time_spent } = progress
 
-  const { error } = await supabase
-    .from('user_reading_progress')
-    .update({
-      // Use the field names you expect in your table. In this case,
-      // we update current_paragraph, started_at, last_updated_at, completed_at, total_time_spent, and status.
-      current_paragraph: paragraph_no,
-      paragraph_id,
-      last_updated_at,
-      // completed_at,
-      // total_time_spent,
-      status,
-    })
-    .eq('book_id', book_id)
-    .eq('user_id', user_id)
+  if (state === 'UPDATED') {
+    const { error } = await supabase
+      .from('user_reading_progress')
+      .update({
+        current_paragraph: paragraph_no,
+        paragraph_id,
+        last_updated_at,
+        ...(total_time_spent !== undefined && { total_time_spent }),
+        status: 'UPDATED'
+      })
+      .eq('book_id', book_id)
+      .eq('user_id', user_id)
 
-  if (error) {
-    throw new Error(error.message)
+    if (error) {
+      throw new Error(error.message)
+    }
+    
+  } else {
+    const { error } = await supabase
+      .from('user_reading_progress')
+      .update({
+        current_paragraph: paragraph_no,
+        paragraph_id,
+        last_updated_at,
+        ...(completed_at && { completed_at }),
+        ...(total_time_spent !== undefined && { total_time_spent }),
+        status: 'COMPLETED'
+      })
+      .eq('book_id', book_id)
+      .eq('user_id', user_id)
+
+    if (error) {
+      throw new Error(error.message)
+    }
   }
 }
 
