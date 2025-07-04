804d08dc6e3baf0ed7de311c2d5f5a3935861bab
SimbaWANGU <simbawangulu@gmail.com>
2025-07-04 14:55:53 +0300

    update project configurations

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,18 @@
-import 'dotenv/config'
-
 export default {
   expo: {
     name: 'Book Worms',
     slug: 'book-worms',
     scheme: 'book-worms',
     version: '0.1.1',
-    icon: './assets/images/bookworms-logo.png',
+    icon: './assets/images/app_icon.png',
     orientation: 'portrait',
     userInterfaceStyle: 'automatic',
-    newArchEnabled: true,
     runtimeVersion: '1.0.0',
+    assetBundlePatterns: [
+      "**/*"
+    ],
     ios: {
-      googleServicesFile: process.env.GOOGLE_SERVICES_IOS,
+      googleServicesFile: "./GoogleService-Info.plist",
       supportsTablet: true,
       usesAppleSignIn: true,
       bundleIdentifier: 'com.simberella.bookie',
@@ -21,8 +21,9 @@ export default {
       }
     },
     android: {
-      googleServicesFile: process.env.GOOGLE_SERVICES_ANDROID,
+      googleServicesFile: "./google-services.json",
       softwareKeyboardLayoutMode: 'pan',
+      edgeToEdgeEnabled: true,
       adaptiveIcon: {
         foregroundImage: './assets/images/bookworms2048.png',
         backgroundColor: '#002B36'
@@ -33,25 +34,23 @@ export default {
       ],
       package: 'com.simberella.bookie'
     },
-    web: {
-      bundler: 'metro',
-      output: 'static',
-      favicon: './assets/images/favicon.png'
-    },
     plugins: [
       'expo-router',
       'expo-font',
       'expo-secure-store',
       'expo-apple-authentication',
+      '@react-native-google-signin/google-signin',
       'expo-asset',
       [
         'expo-build-properties',
         {
-          ios: {
-            newArchEnabled: true
-          },
           android: {
-            newArchEnabled: true
+            compileSdkVersion: 35,
+            targetSdkVersion: 35,
+            minSdkVersion: 27
+          },
+          ios: {
+            useFrameworks: 'static'
           }
         }
       ],
@@ -75,22 +74,17 @@ export default {
         {
           backgroundColor: "#002B36",
           image: './assets/images/bookworms-logo.png',
-          imageWidth: 200,          
+          imageWidth: 200,    
+          resizeMode: 'contain'      
         }
-      ]
+      ],
+      'react-native-edge-to-edge'
     ],
     experiments: {
       typedRoutes: true
     },
-    splash: {
-      
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff'
-    },
     extra: {
-      router: {
-        origin: false
-      },
+      router: {},
       eas: {
         projectId: '8757d84a-3a37-4f3a-a3b4-87ce159a960a'
       },
